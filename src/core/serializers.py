3376c8b0ae657f8c07e34738b2b98807395fb9d4
Rostyslav <malanyukrostik@gmail.com>
2022-03-21 18:43:06 +0200

    add serializer

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
-from core.models import Athlet
 
-class AthletSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Athlet
-        fields = ("name", "about", "category")
+class AthletSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+    about = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    category_id = serializers.IntegerField()
--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,3 @@
-from django.forms import model_to_dict
-from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from core.models import Athlet
@@ -7,13 +5,12 @@ from core.serializers import AthletSerializer
 
 class AthletsAPIView(APIView):
     def get(self, request):
-        lst = Athlet.objects.all().values()
-        return Response({'posts': list(lst)})
+        a = Athlet.objects.all()
+        return Response({'posts': AthletSerializer(a, many=True).data})
 
     def post(self, request):
-        post_new = Athlet.objects.create(name=request.data['name'], about=request.data['about'], category_id=request.data['category_id'])
-        return Response({'post': model_to_dict(post_new)})
+        serializer = AthletSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-# class AthletsAPIView(ListAPIView):
-#     queryset = Athlet.objects.all()
-#     serializer_class = AthletSerializer
+        post_new = Athlet.objects.create(name=request.data['name'], about=request.data['about'], category_id=request.data['category_id'])
+        return Response({'post': AthletSerializer(post_new).data})
