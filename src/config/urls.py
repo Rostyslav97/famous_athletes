73ac47ae6a63121605b3b4fda46dd16d8ab667d2
Rostyslav <malanyukrostik@gmail.com>
2022-03-23 12:33:45 +0200

    routers

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,10 +1,27 @@
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path, include
 from core.views import AthletViewSet
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter, SimpleRouter, Route
 
-router = SimpleRouter()
-router.register(r'athlet', AthletViewSet)
+class MyCustomRouter(SimpleRouter):
+    routes = [
+        Route(url=r'^{prefix}$',
+            mapping={'get': 'list'},
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}),
+        Route(url=r'^{prefix}/{lookup}$',
+            mapping={'get': 'retrieve'},
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Detail'})
+    ]
+
+
+router = MyCustomRouter()
+router.register(r'athlet', AthletViewSet, basename='athlet')
+print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- src/core/views.py
+++ src/core/views.py
@@ -1,8 +1,26 @@
-from rest_framework.viewsets import ModelViewSet
-from core.models import Athlet
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.decorators import action
+from core.models import Athlet, Category
 from core.serializers import AthletSerializer
+from rest_framework.response import Response
 
-
-class AthletViewSet(ModelViewSet):
-    queryset = Athlet.objects.all()
+class AthletViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   GenericViewSet):
+    # queryset = Athlet.objects.all()
     serializer_class = AthletSerializer 
+
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+        if not pk:
+            return Athlet.objects.all()[:3]
+        return Athlet.objects.filter(pk=pk)
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
