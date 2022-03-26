a585c6ce8588dc945faa1938779f032832f1ab71
Rostyslav <malanyukrostik@gmail.com>
2022-03-26 15:03:02 +0200

    add pagination

--- src/core/views.py
+++ src/core/views.py
@@ -4,12 +4,20 @@ from core.models import Athlet
 from core.serializers import AthletSerializer
 from core.permissions import IsAdminOrReadOnly
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import PageNumberPagination
+
+
+class AthletsAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
 
 
 class AthletsListCreateAPI(ListCreateAPIView):
     queryset = Athlet.objects.all()
     serializer_class = AthletSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    pagination_class = AthletsAPIListPagination
 
 
 class AthletsRetrieveUpdateAPI(RetrieveUpdateAPIView):
